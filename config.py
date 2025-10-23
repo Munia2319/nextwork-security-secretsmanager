01b9d7af7e3cd20fce4d48744b0c342d3a9c8195
Munia2319 <mushfikaikfatmunia@gmail.com>
2025-10-23 18:03:18 +0200

    Resolved merge conflicts

--- config.py
+++ config.py
@@ -1,11 +1,6 @@
 # config.py - TEMPORARY for demonstration only
 # WARNING: This is NOT safe for production! We'll fix it with Secrets Manager.
 
-<<<<<<< HEAD
-AWS_ACCESS_KEY_ID = "YOUR_ACTUAL_ACCESS_KEY_ID"
-AWS_SECRET_ACCESS_KEY = "YOUR_ACTUAL_SECRET_ACCESS_KEY"
-AWS_REGION = "us-east-2"
-=======
 import boto3
 import json
 from botocore.exceptions import ClientError
